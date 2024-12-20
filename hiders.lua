c451c643a1253e2b2b0688f45836a0a50b62a04c
compuserscripts <186947602+compuserscripts@users.noreply.github.com>
2024-12-20 15:16:34 +0200

    Update hiders.lua

--- hiders.lua
+++ hiders.lua
@@ -7,7 +7,7 @@ local CONFIG = {
     timeToMarkAsHider = 5.5,  -- Seconds player must remain stationary
     updatePositionThreshold = 1.0,  -- Units player must move to be considered "moving"
     maxDistance = 1650,  -- Maximum distance to check for hiders (hammer units)
-    cleanupInterval = 0.5  -- Clean invalid players every 500ms
+    cleanupInterval = 1.0  -- Clean invalid players every second
 }
 
 -- Player tracking data
@@ -46,19 +46,19 @@ local function CleanInvalidTargets()
     
     nextCleanupTime = currentTime + CONFIG.cleanupInterval
     
+    -- Only clean up if we have data to clean
+    if next(playerData) == nil then return end
+    
     local localPlayer = entities.GetLocalPlayer()
     if not localPlayer then return end
     
     for playerIndex, data in pairs(playerData) do
         local entity = entities.GetByIndex(playerIndex)
         
-        -- Clean if entity is invalid, dead, dormant, on our team, or hasn't been seen recently
+        -- Remove data only if entity is completely invalid or definitely not in play
         if not entity or 
-           not entity:IsValid() or 
            not entity:IsAlive() or
-           entity:IsDormant() or
-           entity:GetTeamNumber() == localPlayer:GetTeamNumber() or
-           currentTime - data.lastMoveTime > CONFIG.cleanupInterval * 2 then
+           entity:GetTeamNumber() == localPlayer:GetTeamNumber() then
             playerData[playerIndex] = nil
         end
     end
@@ -76,22 +76,26 @@ end
 
 -- Update player data
 local function UpdatePlayerData(player)
-    if not player:IsAlive() or player:IsDormant() then return end
+    if not player or not player:IsAlive() then return end
     
     local currentTime = RealTime()
     local currentPos = player:GetAbsOrigin()
     local playerIndex = player:GetIndex()
     
+    -- Initialize new player data
     if not playerData[playerIndex] then
         playerData[playerIndex] = {
             lastPosition = currentPos,
             lastMoveTime = currentTime,
-            isHider = false
+            isHider = false,
+            lastUpdateTime = currentTime
         }
         return
     end
     
     local data = playerData[playerIndex]
+    data.lastUpdateTime = currentTime
+    
     local distance = DistanceBetweenVectors(currentPos, data.lastPosition)
     
     if distance > CONFIG.updatePositionThreshold then
@@ -103,9 +107,51 @@ local function UpdatePlayerData(player)
     end
 end
 
+local function DrawPlayerESP(player, data)
+    local playerPos = player:GetAbsOrigin()
+    local screenW, screenH = GetScreenSize()
+    local centerX, centerY = floor(screenW / 2), floor(screenH / 2)
+    
+    -- Draw box
+    if CONFIG.showBoxes then
+        local mins = player:GetMins()
+        local maxs = player:GetMaxs()
+        
+        if mins and maxs then
+            local bottomPos = Vector3(playerPos.x, playerPos.y, playerPos.z + mins.z)
+            local topPos = Vector3(playerPos.x, playerPos.y, playerPos.z + maxs.z)
+            
+            local screenBottom = WorldToScreen(bottomPos)
+            local screenTop = WorldToScreen(topPos)
+            
+            if screenBottom and screenTop then
+                local height = screenBottom[2] - screenTop[2]
+                local width = height * 0.75
+                
+                local x1 = floor(screenBottom[1] - width / 2)
+                local y1 = floor(screenTop[2])
+                local x2 = floor(screenBottom[1] + width / 2)
+                local y2 = floor(screenBottom[2])
+                
+                Color(unpack(CONFIG.boxColor))
+                OutlinedRect(x1, y1, x2, y2)
+            end
+        end
+    end
+    
+    -- Draw tracer
+    if CONFIG.showTracers then
+        local screenPos = WorldToScreen(playerPos)
+        if screenPos then
+            Color(unpack(CONFIG.tracerColor))
+            Line(centerX, screenH, screenPos[1], screenPos[2])
+        end
+    end
+end
+
 local function OnDraw()
     local localPlayer = entities.GetLocalPlayer()
-    if not localPlayer then return end
+    if not localPlayer or not localPlayer:IsAlive() then return end
     
     -- Check for respawn
     local currentLifeState = localPlayer:GetPropInt("m_lifeState")
@@ -117,65 +163,23 @@ local function OnDraw()
     -- Clean up invalid targets periodically
     CleanInvalidTargets()
     
+    -- Process players
     local players = entities.FindByClass("CTFPlayer")
-    local screenW, screenH = GetScreenSize()
-    local centerX, centerY = floor(screenW / 2), floor(screenH / 2)
-    
     for _, player in pairs(players) do
-        -- Skip dormant players immediately
-        if player:IsDormant() then goto continue end
-        
-        if player:IsAlive() and 
+        if player and player:IsAlive() and 
            player:GetTeamNumber() ~= localPlayer:GetTeamNumber() and
            IsPlayerInRange(player, localPlayer) then
             
             UpdatePlayerData(player)
             
             local data = playerData[player:GetIndex()]
-            if data and data.isHider then
-                local playerPos = player:GetAbsOrigin()
-                
-                -- Draw box
-                if CONFIG.showBoxes then
-                    local mins = player:GetMins()
-                    local maxs = player:GetMaxs()
-                    
-                    if mins and maxs then
-                        local bottomPos = Vector3(playerPos.x, playerPos.y, playerPos.z + mins.z)
-                        local topPos = Vector3(playerPos.x, playerPos.y, playerPos.z + maxs.z)
-                        
-                        local screenBottom = WorldToScreen(bottomPos)
-                        local screenTop = WorldToScreen(topPos)
-                        
-                        if screenBottom and screenTop then
-                            local height = screenBottom[2] - screenTop[2]
-                            local width = height * 0.75
-                            
-                            local x1 = floor(screenBottom[1] - width / 2)
-                            local y1 = floor(screenTop[2])
-                            local x2 = floor(screenBottom[1] + width / 2)
-                            local y2 = floor(screenBottom[2])
-                            
-                            Color(unpack(CONFIG.boxColor))
-                            OutlinedRect(x1, y1, x2, y2)
-                        end
-                    end
-                end
-                
-                -- Draw tracer
-                if CONFIG.showTracers then
-                    local screenPos = WorldToScreen(playerPos)
-                    if screenPos then
-                        Color(unpack(CONFIG.tracerColor))
-                        Line(centerX, screenH, screenPos[1], screenPos[2])
-                    end
-                end
+            if data and data.isHider and not player:IsDormant() then
+                DrawPlayerESP(player, data)
             end
         end
-        ::continue::
     end
 end
 
--- Clean up on script unload
-callbacks.Register("Unload", "HiderESPCleanup", ResetPlayerData)
+-- Register callbacks
 callbacks.Register("Draw", "HiderESP", OnDraw)
+callbacks.Register("Unload", "HiderESPCleanup", ResetPlayerData)
